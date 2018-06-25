d9430b5668f449ff60b0ca2f2d3dfd97607b4423
Adam Moore <adammoore@Adams-MacBook-Pro.local>
2018-06-25 16:45:11 -0600

    Initial commit

--- War App/ViewController.swift
+++ War App/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +29,31 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        
+        
+        if leftRandomNumber > rightRandomNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        } else if rightRandomNumber > leftRandomNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        } else if rightRandomNumber == leftRandomNumber {
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-
